6f935f3732672ebedfd7acb8e166aa972c0d77be
sandoellytonj <sandoellytonj@gmail.com>
2022-09-02 20:35:14 -0300

    started project

---
+++ src/test/java/academy/gama/ClinicaVetApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.gama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
